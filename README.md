aaa0baaf2c89d77279f8d3ddd4faa70d8c43c5c5
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-03-22 17:46:06 -0400

    Reorganized data structure, now everytihng is object oriented!

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ LocalTodos
 
 I'm getting on a flight and I don't want to have to pay for wifi just to use parse so I'm going to build a local version of the Todo app I've been working on.
 
-It will be A simple todo list application without persistence, without user accounts, without priorities. Back to basics.
+It will be a simple todo list application without persistence, without user accounts, without priorities. Back to basics.
+
 
 
--- js/todo.js
+++ js/todo.js
@@ -1,35 +1,47 @@
-
-//provides submit button functionality
-$(document).ready(function(){
-	$(".draft").focus();
-
-
-	$("#draft").keypress(function(event){					//enter key
-		if(event.charCode === 13){
-			Todo.add($("#draft").val());
-			$("#draft").val('');
-		}
-	});
-
-	$("#add").click(function(){							//submit button
-		Todo.add($("#draft").val());
-		$("#draft").val('');
-		$(".draft").focus();
-	})
-
-});
-
+//Todo object
 var Todo = {
-	list: []
+	list: new Array(),
+
+	//returns all tasks in list
+	getList: function(){
+		var ret = "";
+		for(var i = 0; this.list.length; i++){
+			ret += this.list[i].getTask();
+		}
+		return ret;
+	},
+
+	//adds task to list
+	add: function(task){
+		if(task && (task !== ' ')){
+			this.list.push(new Task(task));
+		}
+	}
 }
 
-//function to append new item to list
-var display = function(item){
-	$("<li class=\"list-group-item\">" + item + "</li>").hide().appendTo(".list").fadeIn(500);
+
+//Task object
+function Task(task){
+	//instance variable
+	this.task = task;
+
+	//instance methods
+	this.getTask = function(){
+		return this.task;
+	}
+	this.setTask = function(updated){
+		task = updated;
+	}
+} 
+
+
+//method to append new task to list
+var display = function(task){
+	$("<li class=\"list-group-item task\">" + task + "</li>").hide().appendTo(".list").fadeIn(500);
 }
 
 
-//function to load list on load
+//method to load list on load
 Todo.populate = function(){
 	for(var i = 0; i < this.length; i++){
 		display(this.list[i]);
@@ -37,17 +49,47 @@ Todo.populate = function(){
 }
 
 
-//function to add and display new items
-Todo.add = function(item){
-	this.list.push(item);
-	display(item);
+//method to add and display new items
+Todo.add = function(task){
+	if(task && (task !== ' ')){
+		this.list.push(new Task(task));
+		display(task);
+	}
 }
 
 
-//function to remove item
+//method to remove item
 // Todo.remove(){
-	
+
 // }
 
+
+
+//provides interactivity
+$(document).ready(function(){
+	$(".draft").focus();
+
+
+	$("#draft").keypress(function(event){				//submit task via enter key
+		if(event.charCode === 13){
+			Todo.add($("#draft").val());
+			$("#draft").val('');
+		}
+	});
+
+
+	$("#add").click(function(){							//submit task via submit button
+		Todo.add($("#draft").val());
+		$("#draft").val('');
+		$(".draft").focus();
+	});
+
+
+	$(".task").click(function(){						//removes task
+		var task = $(this).text();
+		alert(task);
+	});
+
+});
 
 
